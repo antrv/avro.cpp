0a196bafd8d6ea7140340cc685b7cf13596b3463
Anton Ryzhov <anton.ryzhov@vertoanalytics.com>
2019-08-14 16:39:13 +0300

    Autodetecting VS path and Win SDK path

--- compile.cl.cmd
+++ compile.cl.cmd
@@ -1,4 +1,5 @@
 @echo off
+setlocal enabledelayedexpansion
 
 set CONFIG=%1
 IF "%1"=="" (
@@ -6,15 +7,8 @@ IF "%1"=="" (
 )
 
 set ROOT=%~dp0
-set WINSDKROOT=C:\Program Files (x86)\Windows Kits\10
-set WINSDKVER=10.0.18362.0
-set WINSDK_INCLUDE=%WINSDKROOT%\Include\%WINSDKVER%
-set WINSDK_LIB=%WINSDKROOT%\Lib\%WINSDKVER%
-set TOOLSET=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.22.27905
-set CXX_COMPILER=%TOOLSET%\bin\Hostx64\x64\cl.exe
 set OUTPUT_PATH=.bin\%CONFIG%
 set IMMEDIATE_PATH=.tmp\%CONFIG%
-
 if not exist %IMMEDIATE_PATH% (
   mkdir %IMMEDIATE_PATH%
 )
@@ -22,6 +16,45 @@ if not exist %IMMEDIATE_PATH% (
 if not exist %OUTPUT_PATH% (
   mkdir %OUTPUT_PATH%
 )
+
+rem Get VS installation folder
+set VSWHEREPATH=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe
+for /F "tokens=* USEBACKQ" %%F in (`"%VSWHEREPATH%" -latest -property installationPath`) do (
+  set VSROOTPATH=%%F
+)
+
+rem Get VC tools installation folder and cl.exe path
+set /p VCTOOLSVERSION=<"%VSROOTPATH%\VC\Auxiliary\Build\Microsoft.VCToolsVersion.default.txt"
+set VCTOOLSPATH=%VSROOTPATH%\VC\Tools\MSVC\%VCTOOLSVERSION%
+set CXX_COMPILER=%VCTOOLSPATH%\bin\Hostx64\x64\cl.exe
+
+rem Get Windows 10 SDK installation folder
+for /F "tokens=1,2*" %%i in ('reg query "HKLM\SOFTWARE\Wow6432Node\Microsoft\Microsoft SDKs\Windows\v10.0" /v "InstallationFolder"') DO (
+    if "%%i"=="InstallationFolder" (
+        SET WINSDKROOT=%%~k
+    )
+)
+for /F "tokens=1,2*" %%i in ('reg query "HKLM\SOFTWARE\Wow6432Node\Microsoft\Microsoft SDKs\Windows\v10.0" /v "ProductVersion"') DO (
+    if "%%i"=="ProductVersion" (
+        SET WINSDKVER=%%~k
+    )
+)
+
+if "%WINSDKROOT%"=="" (
+  for /F "tokens=1,2*" %%i in ('reg query "HKLM\SOFTWARE\Microsoft\Microsoft SDKs\Windows\v10.0" /v "InstallationFolder"') DO (
+      if "%%i"=="InstallationFolder" (
+          SET WINSDKROOT=%%~k
+      )
+  )
+  for /F "tokens=1,2*" %%i in ('reg query "HKLM\SOFTWARE\Microsoft\Microsoft SDKs\Windows\v10.0" /v "ProductVersion"') DO (
+      if "%%i"=="ProductVersion" (
+          SET WINSDKVER=%%~k
+      )
+  )
+)
+
+set WINSDK_INCLUDE=%WINSDKROOT%Include\%WINSDKVER%.0
+set WINSDK_LIB=%WINSDKROOT%Lib\%WINSDKVER%.0
 
 rem Various
 set CL=/utf-8 /Y- /W4 /permissive- /FC
@@ -34,14 +67,14 @@ set CL=%CL% /std:c++17 /TP /Gm- /Zi
 rem Assembly listing
 set CL=%CL% /Fa%IMMEDIATE_PATH%/app.asm /FAsu
 rem Includes
-set CL=%CL% "/I%TOOLSET%\include" "/I%WINSDK_INCLUDE%\ucrt" "/I.\include"
+set CL=%CL% "/I%VCTOOLSPATH%\include" "/I%WINSDK_INCLUDE%\ucrt" "/I.\include"
 rem Output
 set CL=%CL% /Fe%OUTPUT_PATH%\app.exe /Fd%OUTPUT_PATH%\app.pdb /Fo%IMMEDIATE_PATH%\app.obj /Fm%OUTPUT_PATH%\app.map 
 
 rem Linker
 set LINK=/DEBUG /MACHINE:X64 /SUBSYSTEM:CONSOLE /NXCOMPAT /DYNAMICBASE /INCREMENTAL:NO /TLBID:1
 rem Libraries
-set LIB=%TOOLSET%\lib\x64;%WINSDK_LIB%\ucrt\x64;%WINSDK_LIB%\um\x64
+set LIB=%VCTOOLSPATH%\lib\x64;%WINSDK_LIB%\ucrt\x64;%WINSDK_LIB%\um\x64
 
 if "%CONFIG%"=="Debug" (
   set CL=%CL% /MDd /Od /JMC
@@ -52,11 +85,8 @@ if "%CONFIG%"=="Release" (
   set LINK=%LINK% /RELEASE /OPT:REF /OPT:ICF
 )
 
-rem Internal sources
-set SOURCES=
-rem Library sources
-rem set SOURCES=%SOURCES% src/bigint.cpp
-rem Main sources
+rem Sources
 set SOURCES=%SOURCES% src/main.cpp
 
+rem Compile
 "%CXX_COMPILER%" /nologo %SOURCES%
